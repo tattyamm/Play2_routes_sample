5a653d72803f4ba64aca8cb5306110b3445bb76b
tattyamm <entatsu.appli+github@gmail.com>
2012-08-18 17:06:44 +0900

    デーモン化の準備

---
+++ app/controllers/Application.scala
@@ -0,0 +1,35 @@
+package controllers
+
+import org.apache.commons.daemon.Daemon
+import org.apache.commons.daemon.DaemonContext
+import play.core.server.NettyServer
+import play.core.StaticApplication
+import java.io.File
+
+class Application extends Daemon {
+
+  var nettyServer: NettyServer = _
+  var applicationPath: File = _
+
+  override def init(dc: DaemonContext) {
+    // DaemonContextのargumentsからパラメータを受け取ればよい。commons-cliを使ってもよいね。
+    applicationPath = new File(dc.getArguments()(0))
+  }
+
+  override def start {
+    // NettyServerに渡しているパラメータはもっとかっこ良くできるけど、とりあえず版
+    nettyServer = new NettyServer(
+      new StaticApplication(applicationPath),
+      Option(System.getProperty("http.port")).map(Integer.parseInt(_)).getOrElse(9000),
+      Option(System.getProperty("http.address")).getOrElse("0.0.0.0"))
+  }
+
+  override def stop {
+    if (nettyServer != null) nettyServer.stop()
+  }
+
+  override def destroy {
+    nettyServer = null
+  }
+
+}
--- project/Build.scala
+++ project/Build.scala
@@ -9,6 +9,7 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       // Add your project dependencies here,
+      "commons-daemon" % "commons-daemon" % "1.0.10"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
